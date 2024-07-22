e32a9bc6cabda23d718403961bd6e9114149d6ab
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-07-22 13:18:05 +0300

    Move classes

---
+++ src/businesslogicnew/command/CommandType.java
@@ -0,0 +1,20 @@
+package businesslogicnew.command;
+
+import java.util.Arrays;
+
+public enum CommandType {
+    LOGIN("login");
+
+    private final String name;
+
+    CommandType(String name) {
+        this.name = name;
+    }
+
+    public static CommandType getAsType(String s) {
+        return Arrays.stream(CommandType.values())
+                .filter(t -> t.name.equals(s))
+                .findFirst()
+                .orElse(null);
+    }
+}
