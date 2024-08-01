f7c5b0bf47a1959b9f1132179a5bba83f659f142
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-08-01 20:34:32 +0300

    Add tests

---
+++ test/businesslogicnew/command/commands/LoginWithPasswordTest.java
@@ -0,0 +1,35 @@
+package businesslogicnew.command.commands;
+
+import businesslogic.passwordencryptor.PasswordEncryptor;
+import businesslogicnew.database.UserCredentials;
+import businesslogicnew.database.UserDatabase;
+import businesslogicnew.users.ActiveUsers;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class LoginWithPasswordTest {
+    @Test
+    void testExecuteWorksCorrectly() throws IOException {
+        String username = "pavel123";
+        String password = "myPass";
+        int sessionId = 2;
+        String passwordHash = PasswordEncryptor.encryptPassword(password);
+        UserCredentials u = mock();
+        when(u.passwordHash()).thenReturn(passwordHash);
+        UserDatabase db = mock();
+        when(db.getUser(username)).thenReturn(u);
+        ActiveUsers au = mock();
+        when(au.addSession()).thenReturn(sessionId);
+
+        LoginWithPassword command = new LoginWithPassword(username, password, db, au);
+
+        String res = command.execute();
+
+        assertEquals(sessionId, Integer.parseInt(res));
+    }
+}
