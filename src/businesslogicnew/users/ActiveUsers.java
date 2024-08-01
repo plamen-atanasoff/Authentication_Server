27321b13e9fdb64574418ffd04388af65888faf5
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-08-01 15:49:25 +0300

    Add logic for session timeout, add methods for adding and removing session

--- src/businesslogicnew/users/ActiveUsers.java
+++ src/businesslogicnew/users/ActiveUsers.java
@@ -1,20 +1,57 @@
 package businesslogicnew.users;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ActiveUsers {
+    private static final long DEFAULT_SESSION_TIMEOUT_SECONDS = 60 * 5;
+
     private int sessionId = 1;
-    private final long INVALIDATION_INTERVAL;
-    private final long SESSION_TIMEOUT;
-    private final Map<Integer, User> activeUsers;
+
+    private final long sessionTimeout;
+
+    private final Set<Integer> activeUsers = new HashSet<>();
+
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
     public ActiveUsers() {
-        this(1000 * 30, 1000 * 60 * 5);
+        this(DEFAULT_SESSION_TIMEOUT_SECONDS);
     }
-    public ActiveUsers(long invalidationInterval, long sessionTimeout) {
-        this.activeUsers = new HashMap<>();
-        this.INVALIDATION_INTERVAL = invalidationInterval;
-        this.SESSION_TIMEOUT = sessionTimeout;
+    public ActiveUsers(long sessionTimeout) {
+        if (sessionTimeout <= 0) {
+            throw new IllegalArgumentException("SessionTimeout must be a positive number");
+        }
+
+        this.sessionTimeout = sessionTimeout;
+    }
+
+    public int addSession() {
+        activeUsers.add(sessionId);
+        scheduleSessionExpiration(sessionId);
+
+        return sessionId++;
+    }
+
+    public boolean removeSession(int sessionId) {
+        return activeUsers.remove(sessionId);
+    }
+
+    private void scheduleSessionExpiration(int sessionId) {
+        executor.schedule(() -> {
+            activeUsers.remove(sessionId);
+        }, sessionTimeout, TimeUnit.SECONDS);
+    }
+
+    public void shutdown() {
+        executor.shutdown();
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        } catch(InterruptedException e) {
+            executor.shutdownNow();
+        }
     }
 }
