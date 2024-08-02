703252904ae269c16b0b6dcfe162169ffc94c968
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-08-02 17:07:31 +0300

    Add id creation for new users

--- src/businesslogicnew/database/UserDatabase.java
+++ src/businesslogicnew/database/UserDatabase.java
@@ -7,29 +7,35 @@ public class UserDatabase {
 
     private static final Path DEFAULT_DATABASE_PATH = Path.of("userDatabase.txt");
 
+    private int id;
+
     private final FileManager fileManager;
 
-    public UserDatabase() {
+    public UserDatabase() throws IOException {
         this(new FileManager(DEFAULT_DATABASE_PATH));
     }
 
-    public UserDatabase(Path databaseFilePath) {
+    public UserDatabase(Path databaseFilePath) throws IOException {
         this(new FileManager(databaseFilePath));
     }
 
-    public UserDatabase(FileManager fileManager) {
+    public UserDatabase(FileManager fileManager) throws IOException {
         this.fileManager = fileManager;
+
+        id = fileManager.readId();
     }
 
-    public UserCredentials getUser(String username) throws IOException {
+    public User getUser(String username) throws IOException {
         return fileManager.readUser(username);
     }
 
-    public void addUser(UserCredentials user) throws IOException {
+    public void addUser(UserCredentials userCredentials) throws IOException {
+        User user = new User(id++, userCredentials, false);
+
         fileManager.writeUser(user);
     }
 
-    public void updateUser(UserCredentials user) throws IOException {
+    public void updateUser(User user) throws IOException {
         fileManager.updateUser(user);
     }
 
