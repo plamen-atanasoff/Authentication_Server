33d5c82dca8c430ba418c4195f72a8fd078b2dfc
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-08-02 17:06:57 +0300

    Fix code

--- test/businesslogicnew/database/UserCredentialsTest.java
+++ test/businesslogicnew/database/UserCredentialsTest.java
@@ -1,5 +1,6 @@
 package businesslogicnew.database;
 
+import businesslogic.passwordencryptor.PasswordEncryptor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,15 +9,16 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class UserCredentialsTest {
     @Test
     void testOfWorksCorrectly() {
-        String line = "2,bobby,\"pass\",Borislav,Petrov,bobi@abv.bg,0";
+        String passwordHashExpected = PasswordEncryptor.encryptPassword("pass");
+
+        String line = "bobby,\"" + passwordHashExpected + "\",Borislav,Petrov,bobi@abv.bg,0";
 
         UserCredentials user = UserCredentials.of(line);
 
-        assertEquals(2, user.id());
         assertEquals("bobby", user.username());
+        assertEquals(passwordHashExpected, user.passwordHash());
         assertEquals("Borislav", user.firstName());
         assertEquals("Petrov", user.lastName());
         assertEquals("bobi@abv.bg", user.email());
-        assertFalse(user.isAdmin());
     }
 }
