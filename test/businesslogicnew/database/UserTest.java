1825ea439a5a8d75d89d133c44fa50bb7460d3ba
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-08-05 16:48:33 +0300

    Replace test for getValues method with test for toString method

--- test/businesslogicnew/database/UserTest.java
+++ test/businesslogicnew/database/UserTest.java
@@ -22,22 +22,13 @@ public class UserTest {
     }
 
     @Test
-    void testGetValuesReturnsCorrectValues() {
+    void testToStringReturnsCorrectValue() {
         String passwordHash = PasswordEncryptor.encryptPassword("mypass");
         String credentialsLine = "plamen40,\"" + passwordHash + "\",Plamen,Petrov,email@abv.bg";
         String line = "1,0," + credentialsLine;
-        UserCredentials credentials = UserCredentials.of(credentialsLine);
 
         User user = User.of(line);
 
-        String[] values = user.getValues();
-
-        assertEquals(1, Integer.parseInt(values[0]));
-        assertFalse(Boolean.parseBoolean(values[1]));
-        assertEquals(credentials.username(), values[2]);
-        assertEquals(credentials.passwordHash(), values[3]);
-        assertEquals(credentials.firstName(), values[4]);
-        assertEquals(credentials.lastName(), values[5]);
-        assertEquals(credentials.email(), values[6]);
+        assertEquals(line, user.toString());
     }
 }
